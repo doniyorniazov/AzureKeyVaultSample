6ba6b35431c1634e863d284a59418fa5f5a6b918
doniyorniazov <doniyor@silkroadprofessionals.com>
2022-09-27 09:23:46 +0500

    commit

--- AzureKeyVaultSample/Program.cs
+++ AzureKeyVaultSample/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace AzureKeyVaultSample;
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureKeyVaultSample;
 class Program
 {
-    static void Main(string[] args)
+    static  void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        string keyVaultURL = "kvUrl";
+        string tenantId = "tenantId";
+        string clientId = "clientId";
+        string clientSecret = "clientSecret";
+
+
+        var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+        var client = new SecretClient(new Uri(keyVaultURL), credential);
+        var secret = client.GetSecret("SQLConnectionString");
+        
+        string sqlConnectionString = secret.Value.Value;
     }
+    
 }
 
